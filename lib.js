9f06ac79c0a82294d6649c94f78fcaa890371661
Maitena Diaz <maitena3k6@gmail.com>
2025-09-18 17:58:53 -0300

    content

--- lib.js
+++ lib.js
@@ -11,6 +11,17 @@ document.querySelectorAll(".img-container > div").forEach((item) => {
     })
 })
 
+
+document.querySelectorAll(".img-container.v2").forEach((item) => {
+    scroll(animate(item, {
+        opacity: [0, 1, 1, 0],
+        filter: ["blur(20px)", "blur(0px)", "blur(0px)", "blur(20px)"],
+    }), {
+        target: item,
+        offset: ["start end", "end end", "start start", "end start"],
+    })
+})
+
 document.querySelectorAll(".img-container > .text-content").forEach((item) => {
     scroll(animate(item, {
         opacity: [0, 1, 1, 0],
